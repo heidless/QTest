bceace9827dca03474fbc6aa83d2c2e538599132
Rob Lockhart <lockhart.r@gmail.com>
2012-03-06 14:02:36 +0000

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,8 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
-
+  before_save :create_remember_token
+  
   validates :name, presence: true, length: { maximum: 50 }
   valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
@@ -20,3 +21,9 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6}
 end
+
+private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
