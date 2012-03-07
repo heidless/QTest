23a16341c042b58e6b73551587354688936cd171
Rob Lockhart <lockhart.r@gmail.com>
2012-03-07 10:47:30 +0000

    fixed show redirect to index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @todos = @user.todos.paginate(page: params[:page])
-    redirect_to root_path
   end
 
   def new
